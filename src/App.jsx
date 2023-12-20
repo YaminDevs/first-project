99fff9a6f8df610f0b4f46ae07cf07378c238494
EshuDevs <yamindevs@gmail.com>
2023-12-20 10:15:42 +0100

    added fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import {robots} from './robots'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import React ,{ Component } from 'react'
@@ -8,10 +7,16 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ robots : users }))
+}
 
 onSearchChange = (event) => {
   this.setState({ searchfield: event.target.value })
